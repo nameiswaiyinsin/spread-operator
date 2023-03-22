4b6536b5a03899f5418041900d45323995d9220a
nameiswaiyinsin <waiyinsin@gmail.com>
2023-03-22 20:45:13 +0000

    Add spread operator syntax examples and challenge

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -31,3 +31,40 @@ console.log('Third Object:', obj3);
 // Copying only part of an array/object
 let arr5 = [...arr1, {...obj1}, arr3, "x","y","z"];
 console.log(arr5);
+
+
+
+// Example 1: 
+
+let students = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 63, maths: 87, art: 95},
+    }
+];
+
+
+
+// write code answer here:
+
+let subjects = [...students[0].subjects];
+const update = (item, val) => [...item, val];
+let updatedSubjects = update(subjects, "Electronics");
+
+
+console.log(updatedSubjects);
+console.log(students[0]);
